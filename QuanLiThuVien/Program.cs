6e087feb894213125f99fc8b38fe834153035b48
Kahdeg-15520487 <15520487@gm.uit.edu.vn>
2018-01-14 15:28:31 +0700

    Add crash logging

--- QuanLiThuVien/Program.cs
+++ QuanLiThuVien/Program.cs
@@ -1,7 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 using DataAccess;
@@ -13,11 +10,31 @@ namespace QuanLiThuVien {
 		/// </summary>
 		[STAThread]
 		static void Main() {
+			//setup database
 			Database.InitDatabase("thuvien.db", "secret");
+
+			//setup crash handler
+			AppDomain currentDomain = AppDomain.CurrentDomain;
+			currentDomain.UnhandledException += CurrentDomain_UnhandledException;
 
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
 			Application.Run(new MainForm());
+		}
+
+		private static void CurrentDomain_UnhandledException(object sender, UnhandledExceptionEventArgs e) {
+			Exception ex = (Exception)e.ExceptionObject;
+			string message = new string('=', 20) + Environment.NewLine +
+				ex.Message + Environment.NewLine +
+				ex.StackTrace + Environment.NewLine +
+				ex.TargetSite;
+			string logfilename = "crashlog_" + DateTime.Now.ToString(@"dd_MM_yyyy_HH_mm") + ".txt";
+			try {
+				System.IO.File.WriteAllText(logfilename, message);
+			}
+			catch (Exception exx) {
+				throw exx;
+			}
 		}
 	}
 }
